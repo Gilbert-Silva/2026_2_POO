9b0ab35f17c332afd45a6e38d2fba45d2762f40b
Gilbert-Silva <gilbertif1995@outlook.com>
2026-09-08 13:27:58 +0000

    Retangulo

--- POO_03/ex02.py
+++ POO_03/ex02.py
@@ -1,3 +1,5 @@
+import math
+
 class Triangulo:
     def __init__(self, b, h):  # Construtor
         #self.__b = 0
@@ -20,7 +22,27 @@ class Triangulo:
         return self.__b * self.__h / 2
 
 class Retangulo:
-    pass
+    def __init__(self, b, h):  # Construtor
+        #self.__b = 0
+        #self.__h = 0
+        self.set_base(b)
+        self.set_altura(h)
+    def __str__(self):
+        return f"Triângulo com base {self.__b} e altura {self.__h}"    
+    def set_base(self, v):
+        if v >= 0: self.__b = v
+        else: raise ValueError("Base deve ser positiva")    
+    def set_altura(self, v):
+        if v >= 0: self.__h = v
+        else: raise ValueError("Altura deve ser positiva")
+    def get_base(self):
+        return self.__b        
+    def get_altura(self):
+        return self.__h        
+    def calcular_area(self):
+        return self.__b * self.__h
+    def calcular_diagonal(self):
+        return math.sqrt(self.__b ** 2 + self.__h ** 2)
 
 class UI:
     @staticmethod
