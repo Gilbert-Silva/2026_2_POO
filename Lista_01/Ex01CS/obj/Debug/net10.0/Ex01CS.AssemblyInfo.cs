032d859da9cc9d7a60b6ae064ff27d43eb55009e
Gilbert-Silva <gilbertif1995@outlook.com>
2026-09-11 14:18:08 +0000

    Revisão

--- Lista_01/Ex01CS/obj/Debug/net10.0/Ex01CS.AssemblyInfo.cs
+++ Lista_01/Ex01CS/obj/Debug/net10.0/Ex01CS.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Ex01CS")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4bd7da973227ad22f0b146bfba620abaa721f1fa")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9b0ab35f17c332afd45a6e38d2fba45d2762f40b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Ex01CS")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Ex01CS")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
